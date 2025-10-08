ec2839f1730566e408ff52a298ac98ad75d0a78f
Syed Nizamuddin <syednizam9133@gmail.com>
2025-10-08 09:17:55 +0530

    serverless test implemented

---
+++ backend/api/test.js
@@ -0,0 +1,4 @@
+// This is the entire content for backend/api/test.js
+export default function handler(req, res) {
+  res.status(200).json({ message: 'Success! The simple test function is working.' });
+}
